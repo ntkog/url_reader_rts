3a15a2b35e45b2f96270bd94efd61e1ca1adf120
Jorge Barrachina <nusquema@gmail.com>
2018-01-11 10:38:49 +0100

    Choose speaker from language . Now only spanish and english

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ const LANGUAGE = process.argv[2];
 const URL = process.argv[3];
 const FILENAME = process.argv[4];
 
+console.log(`LANGUAGE: [${LANGUAGE}] , URL: [${URL}, FILENAME: [${FILENAME}.wav]`);
+const SPEAKER = LANGUAGE === "es"
+  ? "Monica"
+  : "Alex";
 superagent
   .get(URL)
   .end(function(err,res){
     let data = extractor.lazy(res.text, LANGUAGE);
-      exec(`say -v Monica -o ${FILENAME}.wav --data-format=LEF32@22050 "${data.text()}"`, function (error, stdOut, stdErr) {
+    let OS_PLATFORM_COMMAND =  /^win/.test(process.platform)
+    ? `voice.exe -f -o "${FILENAME}.wav" "${data.text()}"`
+    : `say -v ${SPEAKER} -o ${FILENAME}.wav --data-format=LEF32@22050 "${data.text()}"`
+      exec(OS_PLATFORM_COMMAND, function (error, stdOut, stdErr) {
           // do what you want!
           console.log(`Text from [${URL} were save in [${FILENAME}]]`)
       });
